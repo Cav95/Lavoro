71f434c88013176a2f208340fb7f6ffb7a0afb90
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-03-21 11:39:27 +0100

    AutoUpDate

--- Script/AperturaBomMassiva.sh
+++ Script/AperturaBomMassiva.sh
@@ -1,7 +1,11 @@
 URLBOM="http://pdm/FUSION/plm.html?ClientID=8604d71f-eac9-4f70-928a-cb146295152c&locale=it&IISIDX=0&contextID=aphferoacf&AddinCmd=PSM&opencode="
 URLCODE="http://pdm/FUSION/plm.html?ClientID=8604d71f-eac9-4f70-928a-cb146295152c&locale=it&IISIDX=0&contextID=skrzzpjtze&AddinCmd=Portal&opencode="
  if [[ $1 == "" && $2 == "" ]]; then
- echo "argomneti sbagliati"
+ echo "argomenti sbagliati"
+ exit 1
+ fi
+ if [[ $1 != "BOM" && $1 != "CODE" ]]; then
+ echo "argomento 1 sbagliato , inserire BOM o CODE"
  exit 1
  fi
  for name in $(cat ./$2);do
