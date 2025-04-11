3ba795a2f36dc2f57d7cbe4658bf5a17253b6589
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-04-11 12:19:31 +0200

    aggiunta ora

--- Cmd/DebugFusion.bat
+++ Cmd/DebugFusion.bat
@@ -2,16 +2,18 @@
 #pushd "\\SRVUT\Transfert\MATTIA CAVINA\"
 :: Ottieni la data corrente
 for /f %%i in ('date /t') do set data=%%i
+for /f %%i in ('time /t') do set ora=%%i
 
 :: Rimuovi il carattere "/"
 set data=%data:/=%
+set ora=%ora::=%
 
 :: Stampa la data formattata
 echo Data corrente: %data%
 :loop
 set "programma=PDM_WCF_CLIENT.exe"
 tasklist /fi "imagename eq %programma%" | find /i "%programma%" >nul
-if errorlevel 1 copy "C:\Fusion DeBug\DeBuG.txt" "\\SRVUT\ut\Documenti Vari UT\Documenti PDM Fusion\DebugMonitoring\%username%_%data%_Debug.txt" & goto exit
+if errorlevel 1 copy "C:\Fusion DeBug\DeBuG.txt" "\\SRVUT\ut\Documenti Vari UT\Documenti PDM Fusion\DebugMonitoring\%username%_%ora%_%data%_Debug.txt" & goto exit
 goto loop
 :exit
 #popd
