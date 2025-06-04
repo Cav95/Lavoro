607fe72e68ce527d77ba38171d203baf7a737c46
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-06-04 09:08:59 +0200

    link to order folder work

--- AliasPowerShell/SpostareCartellaStampe.ps1
+++ AliasPowerShell/SpostareCartellaStampe.ps1
@@ -14,37 +14,37 @@ $myname=$env:USERNAME
 
 if ($NumCommessa -lt 3000) {
 $orderFolder="0_2999"
-} elseif ($number -lt 3200) {
+} elseif ($NumCommessa -lt 3200) {
 $orderFolder="3000_3199"
-} elseif ($number -lt 3400) {
+} elseif ($NumCommessa -lt 3400) {
 $orderFolder="3200_3399"
-} elseif ($number -lt 3600) {
+} elseif ($NumCommessa -lt 3600) {
 $orderFolder="3400_3599"
-} elseif ($number -lt 3800) {
+} elseif ($NumCommessa -lt 3800) {
 $orderFolder="3600_3799"
-} elseif ($number -lt 4000) {
+} elseif ($NumCommessa -lt 4000) {
 $orderFolder="3800_3999"
-} elseif ($number -lt 4200) {
+} elseif ($NumCommessa -lt 4200) {
 $orderFolder="4000_4199"
-} elseif ($number -lt 4400) {
+} elseif ($NumCommessa -lt 4400) {
 $orderFolder="4200_4399"
-} elseif ($number -lt 4500) {
+} elseif ($NumCommessa -lt 4500) {
 $orderFolder="4400-4499"
-} elseif ($number -lt 4600) {
+} elseif ($NumCommessa -lt 4600) {
 $orderFolder="4500-4599"
-} elseif ($number -lt 4700) {
+} elseif ($NumCommessa -lt 4700) {
 $orderFolder="4600-4699"
-} elseif ($number -lt 4800) {
+} elseif ($NumCommessa -lt 4800) {
 $orderFolder="4700-4799"
-} elseif ($number -lt 4900) {
+} elseif ($NumCommessa -lt 4900) {
 $orderFolder="4800-4899"
 } else {
 $orderFolder="4900-4999"
 }
 
-#cd "\\srvut\Commesse\$orderFolder\$NumCommessa*"
-#$p=pwd
-#echo "$p"
+cd "\\srvut\Commesse\$orderFolder\$NumCommessa*"
+$p=pwd
+echo "$p"
 Get-ChildItem -Directory | ForEach-Object {
 $name=$_.Name
 echo $name
