b913d350e349fbe99d6542aad075753fc6bbcdbb
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-06-04 09:04:07 +0200

    if copy officina work in SpostaCartellaCommessa

--- AliasPowerShell/SpostareCartellaStampe.ps1
+++ AliasPowerShell/SpostareCartellaStampe.ps1
@@ -13,44 +13,44 @@ $myname=$env:USERNAME
 # Example of if, elseif, and else in PowerShell
 
 if ($NumCommessa -lt 3000) {
-    $orderFolder="0_2999"
+$orderFolder="0_2999"
 } elseif ($number -lt 3200) {
-    $orderFolder="3000_3199"
+$orderFolder="3000_3199"
 } elseif ($number -lt 3400) {
-    $orderFolder="3200_3399"
+$orderFolder="3200_3399"
 } elseif ($number -lt 3600) {
-    $orderFolder="3400_3599"
+$orderFolder="3400_3599"
 } elseif ($number -lt 3800) {
-    $orderFolder="3600_3799"
+$orderFolder="3600_3799"
 } elseif ($number -lt 4000) {
-    $orderFolder="3800_3999"
+$orderFolder="3800_3999"
 } elseif ($number -lt 4200) {
-    $orderFolder="4000_4199"
+$orderFolder="4000_4199"
 } elseif ($number -lt 4400) {
-    $orderFolder="4200_4399"
+$orderFolder="4200_4399"
 } elseif ($number -lt 4500) {
-    $orderFolder="4400-4499"
+$orderFolder="4400-4499"
 } elseif ($number -lt 4600) {
-    $orderFolder="4500-4599"
+$orderFolder="4500-4599"
 } elseif ($number -lt 4700) {
-    $orderFolder="4600-4699"
+$orderFolder="4600-4699"
 } elseif ($number -lt 4800) {
-    $orderFolder="4700-4799"
+$orderFolder="4700-4799"
 } elseif ($number -lt 4900) {
-    $orderFolder="4800-4899"
+$orderFolder="4800-4899"
 } else {
-    $orderFolder="4900-4999"
+$orderFolder="4900-4999"
 }
 
 #cd "\\srvut\Commesse\$orderFolder\$NumCommessa*"
 #$p=pwd
 #echo "$p"
 Get-ChildItem -Directory | ForEach-Object {
-	$name=$_.Name
-	echo $name
-	Copy-Item -Path "T:\MacroUT\Ordinare stampe.xlsx" -Destination "$mypath\$name\"
+$name=$_.Name
+echo $name
+Copy-Item -Path "T:\MacroUT\Ordinare stampe.xlsx" -Destination "$mypath\$name\"
 if ($name.Contains("Officina")) {
-    Copy-Item -Path "\\srvut\ut\FogliElettronici-Modelli\Stampe Officina.xlsx" -Destination "$mypath\$name\"
+Copy-Item -Path "\\srvut\ut\FogliElettronici-Modelli\Stampe Officina.xlsx" -Destination "$mypath\$name\"
 }
 }
 cd ..
